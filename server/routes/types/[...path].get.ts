ba7193d45f100130fca8048ad8c2dfbc659ac9d6
Michael Karbowiak <michael@karbowiak.dk>
2025-02-22 12:07:43 +0100

    Generate the etag for the types in a similar way

--- server/routes/types/[...path].get.ts
+++ server/routes/types/[...path].get.ts
@@ -1,3 +1,6 @@
+import { getHeader } from 'h3';
+import { generateETagForFile } from '../../utils/hashUtils';
+
 // Load the service_metadata.json into memory
 const serviceMetadata = await (Bun.file('./images/service_metadata.json')).json();
 
@@ -18,21 +21,28 @@ export default defineEventHandler(async (event) => {
 
     // Select the image based on the type
     const imagePath = data[type];
+    const fullImagePath = `./images/${imagePath}`;
+
+    const ifNoneMatch = getHeader(event, 'if-none-match');
+    const etag = await generateETagForFile(fullImagePath);
+    if (ifNoneMatch === etag) {
+        return new Response(null, {
+            status: 304,
+            headers: { 'ETag': etag }
+        });
+    }
 
     // Fetch the image and return it
-    const image = await (Bun.file(`./images/${imagePath}`)).arrayBuffer();
-    const imageLastModified = new Date(Bun.file(`./images/${imagePath}`).lastModified);
-    const imageETag = 'W/"' + image.byteLength.toString() + '"';
-    const imageExpires = new Date(Date.now() + 2592000).toUTCString();
+    const image = await (Bun.file(fullImagePath)).arrayBuffer();
     return new Response(image, {
         headers: {
             'Content-Type': 'image/png',
             'Cache-Control': 'public, max-age=2592000',
             'Vary': 'Accept-Encoding',
-            'ETag': imageETag,
-            'Last-Modified': imageLastModified.toUTCString(),
+            'ETag': etag,
+            'Last-Modified': new Date(Bun.file(fullImagePath).lastModified).toUTCString(),
             'Accept-Ranges': 'bytes',
-            'Expires': imageExpires
+            'Expires': new Date(Date.now() + 2592000).toUTCString()
         },
     });
 });
